9b05d4737d1bcb5af3530f3627a1fa645d27d90a
Dominik Váradi <dominik.varadi@wrd.hu>
2022-08-26 14:01:17 +0200

    Initial commit

---
+++ src/main/java/hu/dominikvaradi/sociallybackend/SociallyBackendApplication.java
@@ -0,0 +1,12 @@
+package hu.dominikvaradi.sociallybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SociallyBackendApplication {
+		public static void main(String[] args) {
+			SpringApplication.run(SociallyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/dominikvaradi/sociallybackend/SociallyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.dominikvaradi.sociallybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SociallyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
