c10b5dac2e43eb29232324cca246a205a0835e52
Dominik Váradi <dominik.varadi@wrd.hu>
2022-11-20 13:09:53 +0100

    [BE] Regisztráció visszaadja az access-tokent és refresh-tokent + role-t nem állítjuk be hiba megszűntetése.#15

--- src/main/java/hu/dominikvaradi/sociallybackend/flows/security/domain/enums/Permission.java
+++
@@ -1,6 +0,0 @@
-package hu.dominikvaradi.sociallybackend.flows.security.domain.enums;
-
-public enum Permission {
-	CREATE_POST_OWN,
-	CREATE_POST_ON_OTHER_USER,
-}
--- src/main/java/hu/dominikvaradi/sociallybackend/flows/security/domain/enums/Role.java
+++ src/main/java/hu/dominikvaradi/sociallybackend/flows/security/domain/enums/Role.java
@@ -1,19 +1,5 @@
 package hu.dominikvaradi.sociallybackend.flows.security.domain.enums;
 
-import lombok.Getter;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import static java.util.Arrays.asList;
-
 public enum Role {
-	NORMAL_USER();
-
-	@Getter
-	private final Set<Permission> permissions = new HashSet<>();
-
-	Role(Permission... permissions) {
-		this.permissions.addAll(asList(permissions));
-	}
+	NORMAL_USER
 }
